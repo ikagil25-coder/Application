b1da6d9e7b752fb169ac7df1182eb515a878722d
Markel <ikmostolaza@lhusurbil.eus>
2025-10-07 14:15:55 +0200

    JUnit Test-ak zuzendu

--- Mahaigaina/src/Main1Test.java
+++ Mahaigaina/src/Main1Test.java
@@ -5,8 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class Main1Test {
     @Test
     public void testAgurra() {
-        Main1 main1  = new Main1();
-        String agurra = main1.agurra();
+        String agurra = Main1.agurra();
         assertEquals("Kaixo eta ongietorri Zine Usurbilera!",agurra);
     }
 }
