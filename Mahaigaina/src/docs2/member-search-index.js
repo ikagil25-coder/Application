d71c02a4e86b7cf3018885b2ad232ae85aecb231
Pablo Garrastazu <ikpgarrastazu25@lhusurbil.eus>
2025-10-09 12:39:34 +0200

    testa eta docs sortuta

---
+++ Mahaigaina/src/docs2/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Main2","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Main2","l":"Main2()","u":"%3Cinit%3E()"}];updateSearchResults();
